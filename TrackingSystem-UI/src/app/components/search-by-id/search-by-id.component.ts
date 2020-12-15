30f1207519a945b164168e5bd9c08d6f8171ea03
Udrescu Vlad <udrescumvlad@gmail.com>
2020-12-15 21:59:26 +0200

    search by name added and routing

--- TrackingSystem-UI/src/app/components/search-by-id/search-by-id.component.ts
+++ TrackingSystem-UI/src/app/components/search-by-id/search-by-id.component.ts
@@ -1,24 +1,18 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
 import {FormBuilder} from '@angular/forms';
 import {Router} from '@angular/router';
-import {SearchByIdService} from '../../services/search-by-id/search-by-id.service';
-import {CustomersService} from '../../services/customers/customers.service';
-import { SpecificCustomerService } from '../../services/specific-customer/specific.customer.service';
-import { SpecificCustomerComponent } from '../specific-customer/specific-customer.component';
-import {valueReferenceToExpression} from '@angular/compiler-cli/src/ngtsc/annotations/src/util';
 
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'search-by-id',
+  selector: 'app-search-by-id',
   templateUrl: 'search-by-id.component.html',
   styleUrls: ['./search-by-id.component.css']
 })
 export class SearchByIdComponent implements OnInit {
-  checkoutForm;
+  checkoutFormId;
+
   constructor(private router: Router,
               private formBuilder: FormBuilder) {
-    this.checkoutForm = this.formBuilder.group({
+    this.checkoutFormId = this.formBuilder.group({
       id: ''
     });
   }
@@ -30,7 +24,7 @@ export class SearchByIdComponent implements OnInit {
     this.router.navigate(['/customer/' + value.id]);
 
     console.log(value);
-    this.checkoutForm.reset();
+    this.checkoutFormId.reset();
   }
 
 }
--- TrackingSystem-UI/src/app/components/search-by-name/search-by-name.component.ts
+++ TrackingSystem-UI/src/app/components/search-by-name/search-by-name.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {Form, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-search-by-name',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-by-name.component.css']
 })
 export class SearchByNameComponent implements OnInit {
+  checkoutFormName;
 
-  constructor() { }
+  constructor(private router: Router,
+              private formBuilder: FormBuilder) {
+    this.checkoutFormName = this.formBuilder.group({
+      name: ''
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(value){
+    this.router.navigate(['/customer/name/' + value.name]);
+
+    console.log(value.name);
+    this.checkoutFormName.reset();
+  }
 }
